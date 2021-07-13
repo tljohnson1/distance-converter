eb26202c95dbdaa91d475927b8cbd307aa144548
tljohnson1 <87329727+tljohnson1@users.noreply.github.com>
2021-07-12 20:21:23 -0400

    Update milesfeet.js

--- milesfeet.js
+++ milesfeet.js
@@ -4,11 +4,11 @@ var report = function (feet, miles) {
 };
 
 document.getElementById("m_to_f").onclick = function (miles) {
-    var miles = document.getElementById("distance").value.tofixed(2);
+    var miles = document.getElementById("distance").value;
     report(miles*5280, miles);
 };
 
 document.getElementById("f_to_m").onclick = function (feet) {
-    var feet = document.getElementById("distance").value.tofixed(2);
+    var feet = document.getElementById("distance").value;
     report(feet, feet/5280);
 };
